79654982fd1ef81492f9738c13c71b2cb235e3eb
abhilekhgautam <abhilekhgautam119@gmail.com>
2022-01-28 17:28:05 +0545

    Dir for header files

---
+++ headers/myapp.h
@@ -0,0 +1,17 @@
+//
+// Created by abhilekh on 2022-01-28.
+//
+
+#ifndef ASTRO_GAME_MYAPP_H
+#define ASTRO_GAME_MYAPP_H
+
+#include <wx/wx.h>
+
+class myApp : public wxApp{
+
+public:
+    virtual bool OnInit();
+
+};
+
+ #endif //ASTRO_GAME_MYAPP_H
---
+++ headers/optionframe.h
@@ -0,0 +1,17 @@
+//
+// Created by abhilekh on 2022-01-28.
+//
+
+#ifndef ASTRO_GAME_OPTIONFRAME_H
+#define ASTRO_GAME_OPTIONFRAME_H
+
+#include <wx/wx.h>
+
+class optionFrame : public wxFrame{
+
+public:
+    optionFrame();
+
+};
+
+#endif //ASTRO_GAME_OPTIONFRAME_H
